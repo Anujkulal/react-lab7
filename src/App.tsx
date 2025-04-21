c0825e8fd9abc6c4803847db40fb05cda0a58fb6
anujkulal <anujkulal333@gmail.com>
2025-04-21 15:13:55 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import SampleImg from './sampleImg.webp'
+import './App.css'
+
+interface ProfileCardProps {
+  name: string;
+  bio: string;
+  initialBgColor: string;
+}
+
+const ProfileCard = ({name, bio, initialBgColor}:ProfileCardProps) => {
+  const [bgColor, setBgColor] = useState(initialBgColor);
+  const [hovered, setHovered] = useState(false);
+  const handleMouseEnter = () => {
+    setHovered(true);
+    setBgColor('lightblue');
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(false);
+    setBgColor(initialBgColor);
+  };
+
+  return (
+    <div className={`profile-card ${hovered ? "hovered" : ""}`}
+    style={{ backgroundColor: bgColor, color: hovered ? "#000000" : "#000000" }}
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}
+    >
+      <img src={SampleImg} alt={name} className='profile-pic' />
+      <h2 className='profile-name'>{name}</h2>
+      <p className='profile-bio'>{bio}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+   <div className="app-container">
+    <ProfileCard
+    name="Anonymous"
+    bio="Innovator, entrepreneus, and co-founder of Apple Inc."
+    initialBgColor="linear-gradient(135deg, #add8e6, rgb(146, 23, 23))" />
+   </div>
+  )
+}
+
+export default App
